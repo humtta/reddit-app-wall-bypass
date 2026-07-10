c80f2bfddb9c96d27f3499b716d4ac1101d03ca0
Hugo Marotta <humtta@proton.me>
2026-07-10 16:22:31 -0300

    Add popup selector

---
+++ src/inject.js
@@ -0,0 +1,7 @@
+(() => {
+  const popupTags = [
+    "app-upsell-blocking-bottom-sheet-direct",
+    "app-upsell-blocking-bottom-sheet-seo",
+  ];
+  const popupSelector = popupTags.join(",");
+})();
